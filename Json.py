be45644019488c70a140b492064f847df9a3fd34
Gregory <gregory.zyamamoto@gmail.com>
2026-04-07 21:27:17 -0300

    PAILC
    Documentação inicial

---
+++ Json.py
@@ -0,0 +1,25 @@
+{
+  "input": {
+    "address": "Av. Paulista, 1000",
+    "business_type": "restaurant"
+  },
+  "location": {
+    "lat": -23.561684,
+    "lon": -46.655981
+  },
+  "analysis": {
+    "competitors_count": 12,
+    "poi_count": 8
+  },
+  "score": {
+    "value": 78,
+    "classification": "Localização boa"
+  },
+  "places": [
+    {
+      "name": "Restaurante X",
+      "lat": -23.561,
+      "lon": -46.655
+    }
+  ]
+}
